1b49d194d577845463dde3eb1947640c63d5da9f
Maxolotikashvili <88730788+Maxolotikashvili@users.noreply.github.com>
2022-07-03 21:02:09 +0400

    edit-deploy

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const gameBoard = document.getElementById('game-board')
 function main(currentTime) {
     if (gameOver) {
         if (confirm('You Lost, Press ok To Restart')) {
-            window.location = '/'
+            location.reload();
         } 
         return
     }
